23d2b4682a105f3f81702f2fb0f3a563176280bb
Abel Fernandez <abelfergodevelop@gmail.com>
2020-04-14 19:38:15 +0200

    Add discovery server

---
+++ petclinic-servers/petclinic-discovery-server/src/test/java/com/abeldevelop/petclinic/server/discovery/PetclinicDiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abeldevelop.petclinic.server.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetclinicDiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
